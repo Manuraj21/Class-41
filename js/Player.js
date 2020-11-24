aa60507e4b626656bbf60c55ff5e0f487d1f8ca7
Manuraj21 <kalavalamanuraj@gmail.com>
2020-11-24 18:34:29 +0530

    hi

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -11,6 +12,7 @@ class Player {
       playerCount = data.val();
     })
   }
+  
 
   updateCount(count){
     database.ref('/').update({
@@ -32,4 +34,17 @@ class Player {
       allPlayers = data.val();
     })
   }
+  getCarsAtEnd(){
+    database.ref('CarsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    })
+
+    
+  }
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd:rank
+    });
+  }
+
 }
